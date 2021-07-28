1bdbacf2daafc218084076c57b0b47a8e7e88a30
Aidan O'Reilly <ajoreilly93@gmail.com>
2021-07-28 12:58:02 -0600

    step2: custom styling to match spec sheet

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="student-list-container">
     <div class="student-list" v-for="student in students" :key="student.id">
-      <img :src="student.pic" :alt="'portrait of ' + student.firstName + ' ' + student.lastName">
-      <h3>{{`${student.firstName} ${student.lastName}`}}</h3>
-      <ul>
-        <li>Email: {{student.email}}</li>
-        <li>Company: {{student.company}}</li>
-        <li>Skill: {{student.skill}}</li>
-        <li>Average: {{getAverage(student.grades)}}% </li>
-      </ul>
+      <div class="student-list-image">
+        <img :src="student.pic" :alt="'portrait of ' + student.firstName + ' ' + student.lastName">
+      </div>
+      <div class="student-list-info">
+        <h3>{{`${student.firstName} ${student.lastName}`}}</h3>
+        <ul>
+          <li>Email: {{student.email}}</li>
+          <li>Company: {{student.company}}</li>
+          <li>Skill: {{student.skill}}</li>
+          <li>Average: {{getAverage(student.grades)}}% </li>
+        </ul>
+      </div>
+      
     </div>
   </div>
 </template>
@@ -30,6 +35,7 @@ export default {
       })
     .then((data) => {
         this.students = data.students
+        console.log(data)
       })
     .catch(function(error){
         console.log(error);
@@ -48,6 +54,78 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 
+  .student-list-container {
+    margin: auto;
+    margin-top: 5rem;
+    margin-bottom: 5rem;
+    width: 60vw;
+    height: 83vh;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    /* border: 1px solid gray; */
+    border-radius: 10px;
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 10px;
+
+    &:hover {
+      overflow-y: auto;
+      
+
+      &::-webkit-scrollbar {
+        width: 0.5rem;
+      }
+      &::-webkit-scrollbar-track {
+        background: transparent;
+      }
+      &::-webkit-scrollbar-thumb {
+        background: #aaa3aa;
+        border-radius: 10px;
+      }   
+    }
+
+    .student-list {
+      display: flex;
+      padding: 1rem;
+      flex-direction: row;
+      align-items: center;
+      border-bottom: 2px solid rgb(241, 241, 241);
+
+      .student-list-image {
+        display: flex;
+        justify-content: center;
+
+        img {
+          width: 7rem;
+          border-radius: 100%;
+          border: 1px solid rgb(207, 206, 206);
+        }
+      }
+
+      .student-list-info {
+        margin-left: 2rem;
+
+        h3 {
+          text-transform: uppercase;
+          font-size: 2.3rem;
+          margin-bottom: 0.7rem;
+          margin-top: 0;
+        }
+        ul {
+          margin: 0;
+          padding-left: 1.2rem;
+
+          li {
+          margin: 0;
+          list-style: none;
+          font-weight: 200;
+          line-height: 1.5;
+          }
+        }
+        
+      }
+    }
+
+  }
 </style>
